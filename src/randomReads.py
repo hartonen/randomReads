a3a9b6538f37eec6cdcb2b329b535e2c4b215dcc
Hartonen Tuomo S <thartone@dx0-3044-01.ltdk.helsinki.fi>
2021-05-10 13:32:19 +0300

    Added proteinogenic amino acids as another possible alphabet.

--- src/randomReads.py
+++ src/randomReads.py
@@ -18,15 +18,16 @@ def randomReads():
     parser.add_argument("outfile",help="Output fasta-file name.",type=str)
 
     #OPTIONAL PARAMETERS
-    parser.add_argument("--PFMs",help="Full path(s) to the PFM-file(s). If given, sequences from this PFM are inserted in the middle of the reads (unless another position is specified with --start option). If PFM is not given, random sequences with the given mononucleotide frequencies are produced.",type=str,default=None,nargs='+')
-    parser.add_argument("--seqs",help="Full path to the fasta/fastq-file containing the reads where the PFM is embedded to. If not given, background is generated using the given mononucleotide frequencies.",type=str,default=None)
+    parser.add_argument("--PFMs",help="Full path(s) to the PFM-file(s). If given, sequences from this PFM are inserted in the middle of the reads (unless another position is specified with --start option). If PFM is not given, random sequences with the given alphabet frequencies are produced.",type=str,default=None,nargs='+')
+    parser.add_argument("--seqs",help="Full path to the fasta/fastq-file containing the reads where the PFM is embedded to. If not given, background is generated using the given background alphabet frequencies.",type=str,default=None)
     parser.add_argument("--L",help="Length of the output sequences (default=100, overruled by the sequence length of --seqs if given).",type=int,default=100)
     parser.add_argument("--N",help="Number of sequences generated (default=100000, overruled by the number of sequences in --seqs if given).",type=int,default=100000)
     parser.add_argument("--distance",help="If given, a pair of PFMs is always inserted at a fixed istance from each other (default=None)",type=int,default=None)
     parser.add_argument("--starts",help="Start position(s) of the inserted PFMs (default=random). If multiple PFMs given, each needs to be given its own start position.",type=int,default=None,nargs='+')
-    parser.add_argument("--bgfreqs",help="Background nucleotide frequencies: A, C, G, T (default=0.25,0.25,0.25,0.25).",type=float,default=[0.25,0.25,0.25,0.25],nargs=4)
+    parser.add_argument("--bgfreqs",help="Background alphabet frequencies, default is flat background distribution. Order for DNA: A, C, G, T. Order for protein: A, C, D, E, F, G, H, I, K, L, M, N, P, Q, R, S, T, U, V, W, Y.",type=float,default=None,nargs='+')
     parser.add_argument("--concensus",help="If yes, always insert the concensus of the PWM(s). If no (=defaults), sample from the PFM(s).",type=str,choices=['yes','no'],default='no')
     parser.add_argument("--addToReadName",help="String added to read names to distinguish them from background reads (default=embed).",type=str,default=":embed")
+    parser.add_argument("--alphabet",help="Alphabet used, choices are DNA (=default) or protein.",type=str,choices=['DNA','protein'],default='DNA')
     
     args = parser.parse_args()
 
@@ -128,10 +129,21 @@ def randomReads():
                 w.writerow([header])
                 w.writerow([newseq])
     else:
-        #generating the background sequence from the the given nucleotide frequencies
+        #generating the background sequence from the the given alphabet frequencies
         #creating one args.L length PWM where all sequences are drawn from
-        full_PFM = np.ones(shape=(4,args.L))
-        for i in range(0,4): full_PFM[i,:] *= args.bgfreqs[i]
+        if args.alphabet=='DNA':
+            A = 4 #alphabet length
+        elif args.alphabet=='protein':
+            A = 21 #alphabet length
+        full_PFM = np.ones(shape=(A,args.L))
+        if args.bgfreqs!=None:
+            if len(args.bgfreqs)!=A:
+                print("Wrong number of background frequencies given!")
+                exit
+            for i in range(0,A): full_PFM[i,:] *= args.bgfreqs[i]
+        else:
+            for i in range(0,A): full_PFM[i,:] *= (1.0/A)
+            
         #inserting the PFM-counts if needed
         if args.PFMs!=None:
             for p in range(0,len(args.PFMs)):
@@ -148,9 +160,7 @@ def randomReads():
                 full_PFM[:,starts[p]:starts[p]+Ls[p]] = PFMs[p]
 
 
-        #print(full_PFM)
-        #print(full_PFM.shape)
-        
+                
         #creating the sequences
         with open(args.outfile,'wt') as outfile:
             w = csv.writer(outfile,delimiter='\t')
@@ -160,19 +170,43 @@ def randomReads():
                 PFM_seq = ""
                 #print(randoms)
                 for i in range(0,len(randoms)):
-                    nucl_index = 0
+                    index = 0
                     count = full_PFM[0,i]
                     while True:
                         #print("count="+str(count))
                         #print("random="+str(randoms[i]))
                         if count>=randoms[i]:
-                            if nucl_index==0: PFM_seq += 'A'
-                            elif nucl_index==1: PFM_seq += 'C'
-                            elif nucl_index==2: PFM_seq += 'G'
-                            elif nucl_index==3: PFM_seq += 'T'
-                            break
-                        nucl_index += 1
-                        count += full_PFM[nucl_index,i]
+                            if args.alphabet=='DNA':
+                                if index==0: PFM_seq += 'A'
+                                elif index==1: PFM_seq += 'C'
+                                elif index==2: PFM_seq += 'G'
+                                elif index==3: PFM_seq += 'T'
+                                break
+                            elif args.alphabet=='protein':
+                                if index==0: PFM_seq += 'A'
+                                elif index==1: PFM_seq += 'C'
+                                elif index==2: PFM_seq += 'D'
+                                elif index==3: PFM_seq += 'E'
+                                elif index==4: PFM_seq += 'F'
+                                elif index==5: PFM_seq += 'G'
+                                elif index==6: PFM_seq += 'H'
+                                elif index==7: PFM_seq += 'I'
+                                elif index==8: PFM_seq += 'K'
+                                elif index==9: PFM_seq += 'L'
+                                elif index==10: PFM_seq += 'M'
+                                elif index==11: PFM_seq += 'N'
+                                elif index==12: PFM_seq += 'P'
+                                elif index==13: PFM_seq += 'Q'
+                                elif index==14: PFM_seq += 'R'
+                                elif index==15: PFM_seq += 'S'
+                                elif index==16: PFM_seq += 'T'
+                                elif index==17: PFM_seq += 'U'
+                                elif index==18: PFM_seq += 'V'
+                                elif index==19: PFM_seq += 'W'
+                                elif index==20: PFM_seq += 'Y'
+                                break
+                        index += 1
+                        count += full_PFM[index,i]
                     #print(i)
                     #print(PFM_seq)
                 #saving the new sequence
